7d6ae9c0c380b56068daf1192963af25c43b5c27
Yash <yashdeore5401@gmail.com>
2026-07-01 21:20:47 +0530

    Fix Streamlit deployment issues

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+if "GOOGLE_API_KEY" in st.secrets:
+    GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+else:
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
